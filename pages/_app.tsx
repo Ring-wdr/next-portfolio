10485ef896433b768042b35b3b8c48dfebf251dd
Ring-wdr <akswnd55@gmail.com>
2022-12-16 13:08:29 +0900

    fix: added other og tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>포트폴리오</title>
         <meta name='김밥을 마는 포트폴리오' content='next 죄고' />
         <meta
+          property='og:url'
+          content='https://next-portfolio-ringring.vercel.app/'
+        />
+        <meta property='og:title' content='김만중: 포트폴리오 페이지' />
+        <meta
+          property='og:description'
+          content='프론트엔드 개발자 김만중의 포트폴리오 페이지입니다.'
+        />
+        <meta
           property='og:image'
           content='https://avatars.githubusercontent.com/u/70439662?s=96&v=4'
         />
