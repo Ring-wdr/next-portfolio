247b259e8eebb85b8556c1f5a34602f7cb996cdb
Ring-wdr <akswnd55@gmail.com>
2022-12-16 13:03:35 +0900

    fix: ogimage test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,10 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>포트폴리오</title>
         <meta name='김밥을 마는 포트폴리오' content='next 죄고' />
+        <meta
+          property='og:image'
+          content='https://avatars.githubusercontent.com/u/70439662?s=96&v=4'
+        />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Component {...pageProps} />
