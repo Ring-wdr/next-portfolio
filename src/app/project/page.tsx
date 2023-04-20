4327d1a22de15250011b23bb5f353f8e88e1a452
Ring-Wdr <akswnd55@gmail.com>
2023-04-21 00:59:37 +0900

    og add

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -7,6 +7,17 @@ const DynamicItem = dynamic(
   { ssr: false }
 );
 
+export const metadata = {
+  title: "프로젝트 내역",
+  images: [
+    {
+      url: "https://camo.githubusercontent.com/3414dc60e6294021c229f1cbb8d3c6933ea805681610f933b055dafe3cb61854/68747470733a2f2f696d67312e6461756d63646e2e6e65742f7468756d622f523132383078302f3f73636f64653d6d746973746f72793226666e616d653d68747470732533412532462532466b2e6b616b616f63646e2e6e6574253246646e2532467745336c4325324662747252704453565259592532465270776e46316a6e59504f6834335552306f55594a4b253246696d672e706e67",
+      width: 640,
+      height: 339,
+    },
+  ],
+};
+
 export default function Page() {
   return (
     <div className="d-flex flex-column justify-cc mt-3">
