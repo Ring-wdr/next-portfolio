12a1ab3173fa5ed013636ac456edf3f32afe7ad3
Ring-Wdr <akswnd55@gmail.com>
2023-04-21 00:35:51 +0900

    dynamic import

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,13 +1,18 @@
-import { ProjectItem } from "./ProjectItem";
 import { projectList } from "./projectList";
 import styles from "./index.module.css";
+import dynamic from "next/dynamic";
+
+const DynamicItem = dynamic(
+  () => import("./ProjectItem").then(({ ProjectItem }) => ProjectItem),
+  { ssr: false }
+);
 
 export default function Page() {
   return (
     <div className="d-flex flex-column justify-cc mt-3">
       <div className={styles["proj-list"]}>
         {projectList.map(({ src, title, description }) => (
-          <ProjectItem
+          <DynamicItem
             key={title}
             src={src}
             title={title}
