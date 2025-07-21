75fe166e867e7d0219b72258b575caf51e19949a
ring-wdr <akswnd55@gmail.com>
2025-07-21 23:55:50 +0900

    refactor: react 19에 대응

--- src/shared/ui/form-button.tsx
+++ src/shared/ui/form-button.tsx
@@ -1,33 +1,26 @@
 "use client";
 
-import { ButtonHTMLAttributes, forwardRef } from "react";
 import { useFormStatus } from "react-dom";
 import { classNames } from "@/shared/utils/classnames";
 
-const Button = forwardRef<
-  HTMLButtonElement,
-  ButtonHTMLAttributes<HTMLButtonElement>
->(({ className, ...props }, ref) => {
+export function FormButton({
+  className,
+  ...props
+}: React.ComponentProps<"button">) {
+  const { pending } = useFormStatus();
   return (
     <button
+      type="submit"
       className={classNames(
         "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
         "bg-primary text-primary-foreground hover:bg-primary/90",
         "h-10 px-4 py-2",
         className
       )}
-      ref={ref}
+      disabled={pending}
       {...props}
-    />
-  );
-});
-Button.displayName = "Button";
-
-export const FormButton = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
-  const { pending } = useFormStatus();
-  return (
-    <Button type="submit" disabled={pending} {...props}>
+    >
       {pending ? "Sending..." : "Send"}
-    </Button>
+    </button>
   );
-};
+}
